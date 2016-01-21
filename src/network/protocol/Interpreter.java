a3d48f1155662233aeea17cf6862ae1b156f9829
LiesbethW <e.t.wijers@gmail.com>
2016-01-21 21:58:03 +0100

    Create board from BOARD message

--- src/network/protocol/Interpreter.java
+++ src/network/protocol/Interpreter.java
@@ -1,7 +1,10 @@
 package network.protocol;
 
+import java.util.HashMap;
+
 import exceptions.InvalidArgumentException;
 import exceptions.UnknownCommandException;
+import game.Board;
 import game.Stone;
 
 public class Interpreter implements Constants {
@@ -34,13 +37,37 @@ public class Interpreter implements Constants {
 		}
 	}
 	
-	public static int coordinate(String string) throws InvalidArgumentException {
+	public static int integer(String string) throws InvalidArgumentException {
 		try {
 			return Integer.parseInt(string);
 		} catch (NumberFormatException e) {
 			throw new InvalidArgumentException(
 					String.format("A number was expected, %s was given", string));
 		}
+	}
+	
+	public static Board board(Message message) throws InvalidArgumentException {
+		HashMap<String, Stone> toStone = new HashMap<>();
+		toStone.put(B, Stone.BLACK);
+		toStone.put(W, Stone.WHITE);
+		toStone.put(E, Stone.NONE);
+		
+		int size = (int) Math.sqrt(message.args()[0].length());
+		int blackCaptives = integer(message.args()[1]);
+		int whiteCaptives = integer(message.args()[2]);
+		Stone[][] stones = new Stone[size][size];
+		
+		String[] letters = message.args()[0].split("");
+		int index = 0;
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				stones[i][j] = toStone.get(letters[index]);
+				index++;
+			}
+		}
+		
+		return new Board(stones, blackCaptives, whiteCaptives);
 	}
 	
 }
